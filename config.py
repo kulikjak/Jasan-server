93238b6e74b58d12e4e3105e1229f7ccda21e33e
Jakub Kulík <kulikjak@gmail.com>
2018-01-11 14:12:16 +0100

    Added server settings and config files

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from os import getenv
 APP_NAME = 'Jasan File server'
 
 SERVER_PORT = int(getenv('JASAN_PORT', 80))
-SERVER_ADDRESS = getenv('JASAN_ADDRESS', 'http://localhost:{}/'.format(SERVER_PORT))
+SERVER_ADDRESS = getenv('JASAN_ADDRESS', 'http://jasan.lan/')
 
 FILE_ROOT = getenv('JASAN_FILE_ROOT', 'Jasan')
