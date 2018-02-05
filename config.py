531c483fbd9b2b707964f896873c0a1ecb8a079d
Jakub Kulík <kulikjak@gmail.com>
2018-02-05 08:28:19 +0100

    Fixed config file

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ VERSION = '0.1'
 APP_NAME = 'Jasan File server'
 
 SERVER_PORT = int(getenv('JASAN_PORT', 80))
-SERVER_ADDRESS = getenv('JASAN_SERVER_ADDRESS', 'http://127.0.0.1/')
+SERVER_ADDRESS = getenv('JASAN_SERVER_ADDRESS', 'http://jasan.lan/')
 
 SECRET_KEY = getenv('JASAN_SECRET_KEY', 'S3cr3tK3y')
 MONGODB_URI = getenv('JASAN_MONGODB_URI', 'mongodb://localhost:27017/jasan')
